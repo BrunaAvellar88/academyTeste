9fa717a4b8f29702d565aa65978ebd2655452d04
bamcc <bamcc@10-48-48-118.0200.ufpe.br.iosda.org>
2022-07-11 17:04:03 -0300

    aula 3

---
+++ academyteste/View/PopularCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PopularCollectionViewCell.swift
+//  academyteste
+//
+//  Created by bamcc on 11/07/22.
+//
+
+import UIKit
+
+class PopularCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var image: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    
+}
