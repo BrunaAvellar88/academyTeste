360f234fdda6a413f80c2550916309620f79054e
bamcc <bamcc@MacBook-Pro-76.local>
2022-07-13 14:50:50 -0300

    organizando o código

--- academyteste/View/NowPlayingCollectionViewCell.swift
+++ academyteste/View/NowPlayingCollectionViewCell.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class NowPlayingCollectionViewCell: UICollectionViewCell {
+    
+    
+    static let cellIdentifier = "nowPlayingCell"
+    
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var dateLabel: UILabel!
