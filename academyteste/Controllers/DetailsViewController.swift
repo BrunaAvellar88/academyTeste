08ae1f628bcdb32b30203adbf4294bbe3fa4e06b
bamcc <bamcc@MacBook-Pro-76.local>
2022-07-14 15:34:58 -0300

    adicionando outlets

--- academyteste/Controllers/DetailsViewController.swift
+++ academyteste/Controllers/DetailsViewController.swift
@@ -11,12 +11,28 @@ class DetailsViewController: UIViewController {
     
     var movie: Movie?
 
+    @IBOutlet var backdropImage: UIImageView!
+    @IBOutlet var titlelLabel: UILabel!
+    @IBOutlet var posterImage: UIImageView!
+    @IBOutlet var ratingLabel: UILabel!
+    @IBOutlet var overviewLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        print(movie)
         self.title = movie?.title
+        
+        guard let movie = movie else {
+            return
+        }
+        backdropImage.image = UIImage(named: movie.backdrop)
+        titlelLabel.text = movie.title
+        posterImage.image = UIImage(named: movie.poster)
+        ratingLabel.text = "Rating: \(movie.voteAverage)/10"
+        overviewLabel.text = movie.overview
+        
+
     }
     
 }
